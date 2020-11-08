4e6e7f89b1da88a7ca623a6363a8675c28d36f78
David Borelli <david_borelli@live.com>
2020-11-08 14:48:37 -0300

    refactor: remove knobs and use Controls

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,23 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Main from '.'
-import { withKnobs, text } from '@storybook/addon-knobs'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'React Avançado',
+    description: 'TypeScript, ReactJS, NextJS e Styled Components'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'Example Basic',
+  description: 'TypeScript, ReactJS, NextJS e Styled Components'
+}
+
+export const Default: Story = (args) => <Main {...args} />
+Default.args = {
+  title: 'Example Default',
+  description: 'An example of how to set default props'
+}
